cf68d24645ab7489bb787ca4a4d821cd6c5d5446
NafizUddin <nafizuddin.ctg@gmail.com>
2024-09-30 00:51:49 +0600

    added banner content

--- src/app/(CommonLayout)/_homeSections/Banner/Banner.tsx
+++ src/app/(CommonLayout)/_homeSections/Banner/Banner.tsx
@@ -8,10 +8,16 @@ const Banner = () => {
       <div className={styles.animateZoom} />
       <div className={styles.overlay}>
         {/* Your content goes here */}
-        <h1 className="text-4xl font-bold text-white">
-          Welcome to TravelTrove
+        <h1 className="text-5xl font-bold text-white">
+          Where will you go next?
         </h1>
-        <p className="text-lg text-white">Your adventure begins here!</p>
+        <p className="text-lg text-white mt-6 max-w-2xl mx-auto text-center">
+          🌍 Welcome to TravelTrove! ✈️ Join a vibrant community of travel
+          enthusiasts eager to share their personal stories, exchange tips, and
+          connect with fellow adventurers. Dive into a world of travel
+          inspiration and discover new destinations through the eyes of those
+          who’ve been there. Let’s explore together!
+        </p>
       </div>
     </div>
   );
