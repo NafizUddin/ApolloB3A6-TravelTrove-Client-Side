c5492bf7f804ca8febc3e21dfd629dead50aa2ac
NafizUddin <nafizuddin.ctg@gmail.com>
2024-09-29 20:46:39 +0600

    added headers in about us

---
+++ src/app/(CommonLayout)/about/_sections/Headers.tsx
@@ -0,0 +1,32 @@
+const Headers = () => {
+  return (
+    <div className="w-11/12 mx-auto">
+      <div className="border-t-4 w-[80px] border-backup" />
+      <h1 className="uppercase my-5 xl:text-[20px] tracking-[10px]">
+        We are TravelTrove
+      </h1>
+      <h1 className="font-bold text-[20px] xl:text-4xl my-4">
+        A Platform For <br />
+        <span className="text-primary">Travel Tips & Destination Guides</span>
+      </h1>
+      <h1 className="">
+        <br />
+      </h1>
+      <p className="text-sm xl:text-lg text-justify">
+        TravelTrove is your ultimate platform for exploring the world through
+        insightful travel tips and comprehensive destination guides. Whether
+        you're planning a relaxing getaway, an adventurous exploration, or
+        seeking local insights for your next trip, TravelTrove offers a wealth
+        of information tailored to travelers of all levels. Our resources
+        include expert advice, must-visit attractions, cultural highlights, and
+        practical tips to enhance your journey. Committed to fostering a vibrant
+        travel community, we ensure that every guide is curated with care,
+        helping you make the most of your travel experiences. Choose TravelTrove
+        to unlock a treasure trove of knowledge that will elevate your
+        adventures.
+      </p>
+    </div>
+  );
+};
+
+export default Headers;
--- src/app/(CommonLayout)/about/page.tsx
+++ src/app/(CommonLayout)/about/page.tsx
@@ -1,5 +1,6 @@
 import PageSectionTitle from "@/src/components/ui/section/PageSectionTitle";
 import Team from "./_sections/Team";
+import Headers from "./_sections/Headers";
 
 const About = () => {
   return (
@@ -8,7 +9,7 @@ const About = () => {
         title="About Us"
         description="Learn about our passion and how it all began."
       />
-
+      <Headers />
       <Team />
     </div>
   );
