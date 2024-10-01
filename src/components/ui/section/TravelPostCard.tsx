12aa9e0e547bcd6287719db184c7e40d2181aaa0
NafizUddin <nafizuddin.ctg@gmail.com>
2024-10-01 21:34:35 +0600

    added edit and delete icon in comment

--- src/components/ui/section/TravelPostCard.tsx
+++ src/components/ui/section/TravelPostCard.tsx
@@ -237,49 +237,51 @@ const TravelPostCard = ({ singlePost }: any) => {
                       </div>
                       <p>{comment.text}</p>
                     </div>
-                    <div className="flex gap-3 mr-2">
-                      <Tooltip showArrow={true} content="Edit Comment">
-                        <div className="bg-blue-500 p-1 rounded-full w-8 h-8 flex items-center justify-center">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="white"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-pencil"
-                          >
-                            <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-                            <path d="m15 5 4 4" />
-                          </svg>
-                        </div>
-                      </Tooltip>
-                      <Tooltip showArrow={true} content="Delete Comment">
-                        <div className="bg-blue-500 p-1 rounded-full w-8 h-8 flex items-center justify-center">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="white"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-trash-2"
-                          >
-                            <path d="M3 6h18" />
-                            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                            <line x1="10" x2="10" y1="11" y2="17" />
-                            <line x1="14" x2="14" y1="11" y2="17" />
-                          </svg>
-                        </div>
-                      </Tooltip>
-                    </div>
+                    {comment.user === user?._id && (
+                      <div className="flex gap-3 mr-2">
+                        <Tooltip showArrow={true} content="Edit Comment">
+                          <div className="bg-blue-500 p-1 rounded-full w-8 h-8 flex items-center justify-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="white"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              className="lucide lucide-pencil"
+                            >
+                              <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                              <path d="m15 5 4 4" />
+                            </svg>
+                          </div>
+                        </Tooltip>
+                        <Tooltip showArrow={true} content="Delete Comment">
+                          <div className="bg-blue-500 p-1 rounded-full w-8 h-8 flex items-center justify-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="white"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              className="lucide lucide-trash-2"
+                            >
+                              <path d="M3 6h18" />
+                              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                              <line x1="10" x2="10" y1="11" y2="17" />
+                              <line x1="14" x2="14" y1="11" y2="17" />
+                            </svg>
+                          </div>
+                        </Tooltip>
+                      </div>
+                    )}
                   </div>
                 </div>
               ))}
